1fea8ee2e245931323f1bc357023b394c90e4619
Guillaume Jasmin <guillaume.jasmin@gmail.com>
2019-03-27 13:42:33 +0100

    log disconnection and setMaxListeners to 30

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const twitter = require('./twitter');
 const io = require('./socket');
 const EE = require('events').EventEmitter;
+const showLog = require('./log');
 
 const pubsub = new EE();
+pubsub.setMaxListeners(30);
 
 const stream = twitter.stream('statuses/filter', { track: ['zidane'] });
 
@@ -11,7 +13,12 @@ stream.on('tweet', data => {
 });
 
 io.on('connection', client => {
+  showLog(io);
   pubsub.on('tweet', data => {
     client.emit('tweet', data);
+  });
+
+  client.on('disconnect', () => {
+    showLog(io);
   });
 });
